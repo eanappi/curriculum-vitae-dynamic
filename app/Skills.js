54e5136ddf5ce8ee4399adc5dac0607cd764d027
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-01-28 13:59:57 -0300

    FIX - Corrige identación

--- app/Skills.js
+++ app/Skills.js
@@ -1,25 +1,25 @@
 const Skills = (skills = []) => {
   let template = `
-  <div class="about-skills pt-25">
-    ${skills.map(item => `
-      <div class="skill-item mt-25">
-          <div class="skill-header">
-              <h6 class="skill-title">${item.name}</h6>
-              <div class="skill-percentage">
-                  <div class="count-box counted">
-                      <span class="counter">${item.percentage}</span>
-                  </div>
-                  %
-              </div>
-          </div>
-          <div class="skill-bar">
-              <div class="bar-inner">
-                  <div class="bar progress-line" data-width="${item.percentage}"></div>
-              </div>
-          </div>
-      </div> <!-- skill item -->
-    `).join('')}
-  </div> <!-- about skills -->
+    <div class="about-skills pt-25">
+      ${skills.map(item => `
+        <div class="skill-item mt-25">
+            <div class="skill-header">
+                <h6 class="skill-title">${item.name}</h6>
+                <div class="skill-percentage">
+                    <div class="count-box counted">
+                        <span class="counter">${item.percentage}</span>
+                    </div>
+                    %
+                </div>
+            </div>
+            <div class="skill-bar">
+                <div class="bar-inner">
+                    <div class="bar progress-line" data-width="${item.percentage}"></div>
+                </div>
+            </div>
+        </div> <!-- skill item -->
+      `).join('')}
+    </div> <!-- about skills -->
   `
 
   return template
